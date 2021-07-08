4cd886bd0fe20c511c43216d50fdadd61096b492
yu-yama <yu-yama@users.noreply.github.com>
2021-07-08 14:57:10 +0900

    [chg] SampleTest now iterates over files
    [upd] now tests both opt = true and false cases

--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -16,15 +16,54 @@
 
 #include "bf/bf.hpp"
 #include <gtest/gtest.h>
+#include <iomanip>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
 
-constexpr bool opt = false;
+template <typename opt>
+struct SampleTest : public ::testing::Test{};
 
-TEST(SampleTest, hello) {
+using SampleTest_types = ::testing::Types< std::integral_constant<bool, false>, std::integral_constant<bool, true> >;
+
+TYPED_TEST_SUITE(SampleTest, SampleTest_types);
+
+TYPED_TEST(SampleTest, file) {
     using namespace std;
-    istringstream is;
-    ostringstream os, ds;
-    BFInterpreter::BF<opt> b("++++++[>+++<-]>[>++++>++++++<<-]>.>-------.+++++++..+++.", is, os, ds);
-    b.run();
-    EXPECT_EQ("Hello", os.str());
-    EXPECT_EQ("", ds.str());
+    constexpr static bool opt = TypeParam::value;
+    cerr << "opt: " << boolalpha << opt << '\n';
+    #ifdef BF_SAMPLE_SRC_DIR
+        string fp = BF_SAMPLE_SRC_DIR;
+    #else
+        string fp = "../sample";
+    #endif
+    filesystem::directory_iterator f(fp);
+    for (const auto& s : f) {
+        auto sp = s.path();
+        if (sp.extension() != ".bf") continue;
+        cerr << "testing: " << sp << '\n';
+        ifstream ss(sp);
+        auto ip = sp, oep = sp, dep = sp;
+        ip.replace_extension(".in.txt");
+        oep.replace_extension(".out.txt");
+        dep.replace_extension(string(".debug") + static_cast<char>('0' + opt) + ".txt");
+        ifstream ifs(ip), oefs(oep), defs(dep);
+        if (defs) {
+            defs >> BFInterpreter::BF<opt>::debug;
+            defs.get(); // remove delimiter
+        } else BFInterpreter::BF<opt>::debug = BFInterpreter::BF<opt>::no_debug;
+        auto to_string = [](istream& s) {
+            return s ? string(istreambuf_iterator<char>(s), {}) : "";
+        };
+        istringstream is(to_string(ifs));
+        ostringstream os, ds;
+        BFInterpreter::BF<opt> b(ss, is, os, ds);
+        b.run();
+        EXPECT_EQ(to_string(oefs), os.str());
+        EXPECT_EQ(to_string(defs), ds.str());
+        ss.close();
+        ifs.close();
+        oefs.close();
+        defs.close();
+    }
 }
